6f5065a0a82a7069de8d5fb7b423791a4596b024
Aditya Shinde <ideabliss4@gmail.com>
2026-01-31 12:27:41 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -90,7 +90,7 @@ const Login = () => {
       <main className="flex-grow flex items-center justify-center p-4">
         <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-8 space-y-6">
           <div className="text-center">
-            <h2 className="text-3xl font-bold text-gray-900"> adiiii</h2>
+            <h2 className="text-3xl font-bold text-gray-900"> mohit</h2>
             <p className="text-gray-500 mt-2">Please.</p>
           </div>
 
