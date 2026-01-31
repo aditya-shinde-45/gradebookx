398a5615f526388622f403e2cfd8b327168d0ef3
Aditya Shinde <ideabliss4@gmail.com>
2026-01-31 10:54:55 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
       <header className="bg-gray-100 py-4 px-6 md:px-10">
         <div className="container mx-auto flex items-center">
           <span className="material-icons text-gray-700 text-3xl mr-3">hjjhvhjv</span>
-          <h1 className="text-2xl font-bold text-gray-800">sarthakhjv hjbjn zoro</h1>
+          <h1 className="text-2xl font-bold text-gray-800">sarthakhjv luffy zoro</h1>
         </div>
       </header>
 
