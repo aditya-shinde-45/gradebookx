08d416b3ff12ee3634cb477da1ed89e5493b6932
Aditya Shinde <ideabliss4@gmail.com>
2026-01-31 06:54:38 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -83,7 +83,7 @@ const Login = () => {
       <header className="bg-gray-100 py-4 px-6 md:px-10">
         <div className="container mx-auto flex items-center">
           <span className="material-icons text-gray-700 text-3xl mr-3">auto_stories</span>
-          <h1 className="text-2xl font-bold text-gray-800">depclick hjhkjghvgcvhbjnkjbmnbgjyfgbtest</h1>
+          <h1 className="text-2xl font-bold text-gray-800">depclickzfdexgft hjhkjghvgcvhbjnkjbmnbgjyfgbtest</h1>
         </div>
       </header>
 
